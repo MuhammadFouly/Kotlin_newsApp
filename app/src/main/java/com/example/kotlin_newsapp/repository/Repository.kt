2d01276014756ab84ed6cb5182963c9be9f4d4c4
omarzer0 <omarzero1015@gmail.com>
2022-11-20 22:53:03 +0200

    Fix dependencies, Hilt and Navigation

--- app/src/main/java/com/example/kotlin_newsapp/repository/Repository.kt
+++ app/src/main/java/com/example/kotlin_newsapp/repository/Repository.kt
@@ -2,10 +2,11 @@ package com.example.kotlin_newsapp.repository
 
 import com.example.kotlin_newsapp.di.NewsAPI
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class Repository
-@Inject
-constructor(private val api:NewsAPI) {
-    suspend fun getBreakingNews()=api.getBreakingNews()
-
+@Singleton
+class Repository @Inject constructor(
+    private val api: NewsAPI,
+) {
+    suspend fun getBreakingNews() = api.getBreakingNews()
 }
--- app/src/main/java/com/example/kotlin_newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/kotlin_newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -1,50 +1,54 @@
-package com.example.foulynewsapp.ui.fragments
+package com.example.kotlin_newsapp.ui.fragments
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.foulynewsapp.adapters.NewsAdapter
-import com.example.kotlin_newsapp.databinding.ActivityNewsBinding
+import com.example.kotlin_newsapp.R
+import com.example.kotlin_newsapp.adapters.NewsAdapter
 import com.example.kotlin_newsapp.databinding.FragmentBreakingNewsBinding
-import com.example.kotlin_newsapp.ui.NewsActivity
 import com.example.kotlin_newsapp.ui.NewsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class BreakingNewsFragment:Fragment() {
-    lateinit var binding: FragmentBreakingNewsBinding
-    private val viewmodel:NewsViewModel by viewModels()
-    lateinit var adapt:NewsAdapter
+@AndroidEntryPoint
+// TODO look at the parent fragment constructor...We pass the layout id
+//  instead of overriding onCreateView and init the binding in onViewCreated method
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+    private lateinit var binding: FragmentBreakingNewsBinding
+    private val viewModel: NewsViewModel by viewModels()
+    lateinit var adapt: NewsAdapter
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        binding= FragmentBreakingNewsBinding.inflate(inflater,container,false)
-        return binding.root
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?,
+//    ): View? {
+//        binding = FragmentBreakingNewsBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
 
-    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBreakingNewsBinding.bind(view)
+        Log.e("TestTag", "BreakingNewsFragment: ")
+
         setUpRecyclerView()
 
 
     }
 
     private fun setUpRecyclerView() {
-        adapt=NewsAdapter()
+        adapt = NewsAdapter()
         binding.rvBreakingNews.apply {
-            adapter=adapt
+            adapter = adapt
             setHasFixedSize(true)
         }
-        viewmodel.BreakingNews.observe(viewLifecycleOwner, Observer{ response ->
-            if(response!=null){
+        viewModel.breakingNews.observe(viewLifecycleOwner) { response ->
+            if (response != null) {
                 adapt.differ.submitList(response.articles)
             }
-
-        })
+        }
     }
 
 
