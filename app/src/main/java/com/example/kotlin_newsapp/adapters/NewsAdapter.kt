65718c88d484eecb8a1cdbbd4cfb707a727d8be8
DELL <mohamedfouly14@gmail.com>
2022-11-24 06:14:54 +0200

    adding viewBinding to adapter

--- app/src/main/java/com/example/kotlin_newsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/kotlin_newsapp/adapters/NewsAdapter.kt
@@ -9,24 +9,34 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.kotlin_newsapp.R
+import com.example.kotlin_newsapp.databinding.FragmentBreakingNewsBinding
+import com.example.kotlin_newsapp.databinding.ItemArticlePreviewBinding
 import com.example.kotlin_newsapp.models.Article
 
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
-    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // TODO use viewBinding instead
-        var imageView: ImageView
-        var name: TextView
-        var title: TextView
-        var description: TextView
-        var publishedAt: TextView
+    inner class ArticleViewHolder(
+        val binding:ItemArticlePreviewBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(article: Article) {
+            binding.tvDescription.text = article.description
+            binding.tvPublishedAt.text = article.publishedAt
+            binding.tvTitle.text = article.title
+            binding.tvSource.text = article.source.name
+            Glide.with(binding.ivArticleImage.context).load(article.urlToImage)
+                .placeholder(R.drawable.ic_loading).error(R.drawable.ic_no_image)
+                .transform(CenterCrop(), RoundedCorners(15))
+                .into(binding.ivArticleImage)
 
-        init {
-            imageView = itemView.findViewById(R.id.ivArticleImage)
-            name = itemView.findViewById(R.id.tvSource)
-            title = itemView.findViewById(R.id.tvTitle)
-            description = itemView.findViewById(R.id.tvDescription)
-            publishedAt = itemView.findViewById(R.id.tvPublishedAt)
+            binding.root.setOnClickListener {
+                onItemCliclListener?.let {
+                    it(article)
+                }
+            }
+
+
         }
     }
 
@@ -41,25 +51,15 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     }
     val differ = AsyncListDiffer(this, differCallback)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder =
-        ArticleViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.item_article_preview,
-                parent,
-                false
-            )
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val binding=ItemArticlePreviewBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return ArticleViewHolder(binding)
+    }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val currentArticle = differ.currentList[position]
-        holder.apply {
-            Glide.with(itemView).load(currentArticle.urlToImage).into(imageView)
-            name.text = currentArticle.source.name
-            title.text = currentArticle.title
-            description.text = currentArticle.description
-            publishedAt.text = currentArticle.publishedAt
-            onItemCliclListener?.let { it(currentArticle) }
-        }
+        holder.bind(currentArticle)
     }
 
     override fun getItemCount(): Int = differ.currentList.size
